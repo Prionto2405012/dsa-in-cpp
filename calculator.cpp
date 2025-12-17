90171697613287085aa537b93d280ebc422290d9
Prionto2405012 <nonredoxprionto@gmail.com>
2025-12-17 20:33:02 +0600

    cheer up prionto you did calculator, btw its ndc anniversary

--- calculator.cpp
+++ calculator.cpp
@@ -2,97 +2,94 @@
 #include <stack>
 using namespace std;
 
-bool is_operator(char c) {
-    switch (c) {
-        case '+':
-        case '-':
-        case '*':
-        case '/':return true;
-        default:return false;
-    }
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
 }
-bool is_operator(string s) {
-    return is_operator(s[0]);
-}
-bool higher_order(string s1,string s2) {
-    char c1 = s1[0];//comes form arr
-    char c2 = s2[0];//comes from stack
-    if (c1==c2) return false;
-    if (c1=='/') {
-        return true;
-    }
-    else if (c1=='*') {
-        if (c2 == '+' || c2 == '-') return true;
-        else return false;
-    }
-    else if (c1 == '+') {
-        if (c2 =='-') return true;
-        else return false;
-    }
-    else if (c1=='-') return false;
-}
-string arr[100];
-int element = 0;
 
-bool is_digit(char c) {
-    if (c >= '0' && c <= '9') return true;
-    else return false;
-}
-int main() {
 
-    string str;
-    getline(cin,str);
-
-    if (str.size()==0 || is_operator(str[0])) {
-        cout<<"vag shala";
-        return -1;
-    }
-    int i = 0;
-    while (1) {
-        while (str[i]==' ') i++;
-        if (str[i]== '\0' ) break;
-        if (is_digit(str[i])) {
-            int start = i;
-            while (is_digit(str[i])) {
-                i++;
+string postfix(string infix) {
+    string ans;
+    stack<char> operators;
+    int i=0;
+    while (i<infix.size()) {
+        if (infix[i] ==  ' ');
+        else if (infix[i]>='0' && infix[i]<='9') ans.push_back(infix[i]);
+        else if (infix[i]=='(') operators.push(infix[i]);
+        else if (infix[i]==')') {
+            while (!operators.empty() && operators.top()!='(') {
+                ans.push_back(operators.top());
+                operators.pop();
             }
-            string temp;
-            while (start<i) {
-                temp+=str[start];
-                start++;
-            }
-            arr[element] = temp;
-            element++;
+            operators.pop();
         }
-        else if (is_operator(str[i])) {
-            if (str[i+1]!='\0' && is_operator(str[i+1])) {
-                cout<<"vag shala";
-                return -1;
-            }
-            string temp;
-            temp += str[i];
-            arr[element] = temp;
-            element++;
-            i++;
-        }
-    }
-    for (int j=0;j<element;j++) {
-        cout<<arr[j]<<endl;
-    }
-    //so far tokenization is done
-    //now arr contains the tokens
-    stack<string> postfix,operators;
-    for (int j=0;j<element;j++) {
-        if (!is_operator(arr[j])) {
-            postfix.push(arr[j]);
-        }
-        else if (is_operator(arr[j])) {
-            if (operators.size()==0) {
-                operators.push(arr[j]);
-            }
+        else {
+            if (operators.empty()) operators.push(infix[i]);
+            else if (precedence(infix[i])>precedence(operators.top())) operators.push(infix[i]);
             else {
-
+                while (!operators.empty() && precedence(infix[i])<=precedence(operators.top())) {
+                    ans.push_back(operators.top());
+                    operators.pop();
+                }
+                operators.push(infix[i]);
             }
         }
+        i++;
     }
+    while (!operators.empty()) {
+        ans.push_back(operators.top());
+        operators.pop();
+    }
+    return ans;
 }
+
+double calculate(string str) {
+    stack<double> nums;
+    str = postfix(str);
+    for (int i=0;i<str.size();i++) {
+        char c= str[i];
+        if (c>='0' && c<='9') {
+            nums.push((double)(c-'0'));
+        }
+        else {
+            int op2 = nums.top();
+            nums.pop();
+            int op1 = nums.top();
+            nums.pop();
+            double ans;
+            switch (c) {
+                case '+':
+                    ans = (double) op1+op2;
+                    break;
+                case '-':
+                    ans = (double) op1-op2;
+                    break;
+                case '*':
+                    ans = (double) op1*op2;
+                    break;
+                case '/':
+                    ans = (double) op1/op2;
+                    break;
+            }
+            nums.push(ans);
+        }
+    }
+    return nums.top();
+}
+
+
+int main() {
+    string s = "5 + 3 * 4 -3 ";
+    cout<<calculate(s)<<endl;
+}
+/*Strategy:
+ *1/ if operand is number
+ *  ->add directly to output
+ *2/ if ( -> push to stack
+ *3/ if ) -> while not ( and not empty -> pop and add to output
+ *4/ if operator is -> high precedance -> push to stack
+ *                  -> equal precedence -> pop add push
+ *                  -> while lower precedence -> pop and add -> then push
+ *  Now for calculations
+ */
